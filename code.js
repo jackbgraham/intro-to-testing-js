6adc3174a3a0e1e6cb62972f4d6eb2ddf272703e
Jack Graham <jack.blair.graham@gmail.com>
2022-08-24 12:21:39 -0500

    sayhello

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
 // helloWorld function 
 function helloWorld() {
     return "Hello, World!";
+//    console.log("Hello, World!")
 }
+function sayHello(){
+
+}
+
+
+//exercise 0
+//changing helloWorld to hello causes all the specs to fail
+//changing hello to helloWorld causes all the specs to go green again
+//deleting , World! causes a spec failure
+//changing return to console.log causes 3 spec failures
+//syntax error results in falures
